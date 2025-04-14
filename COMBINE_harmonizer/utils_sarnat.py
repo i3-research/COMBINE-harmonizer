2b787733e6a7ae5b7ed891139acf113e1f963e25
Chuan-Heng Hsiao <Chuan-Heng.Hsiao@childrens.harvard.edu>
2025-04-14 17:06:04 -0400

    Only 5 chronological categories.
    1. update dictionary to 5 chronological categories. distribute the derived variables to the 5 chronological categories.
    2. inv => rank for ordinal types (converting ordinal types to numerical types).
    3. update LICENSE, as MIT for Software, and CC0 for `/results` (Results). copyright owner: COMBINE.
    4. time-slots with unit (minute or day).
    5. imagingTimeSlot => imagingNumber.
    6. more `exclude_columns`.
    7. for MRINRNPatternOfInjuryAvg:
       previous: considering only the 1st 2 readers, as the main MRI readers in the LH and OC trials.
       now: considering all the 3 readers.
    8. add results/README.md.

--- COMBINE_harmonizer/utils_sarnat.py
+++ COMBINE_harmonizer/utils_sarnat.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+pd.options.mode.copy_on_write = True
 import numpy as np
 
 
@@ -10,11 +11,12 @@ def total_modified_sarnat_score(df: pd.DataFrame, columns: list[str]) -> pd.Seri
     '''
     s = pd.Series(data=[np.nan] * len(df), index=df.index)
     for idx, column in enumerate(columns):
-        is_valid = df[column].isnull() == False
-        print(f'[INFO] ({idx}/{len(columns)}) column: {column} valid: ({is_valid.sum()}/{len(df)})')
+        s_column = df[column].astype(np.float64)
+        is_valid = s_column.isnull() == False
+        print(f'[INFO] ({idx}/{len(columns)}) column: {column} ({df[column].dtype}) valid: ({is_valid.sum()}/{len(df)})')  # noqa
 
         is_valid_nan = s.isnull() & is_valid
         s.loc[is_valid_nan] = 0
-        s.loc[is_valid] += df.loc[is_valid, column]
+        s.loc[is_valid] += s_column[is_valid]
 
     return s
